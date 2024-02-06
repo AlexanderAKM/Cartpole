5d3e6907c8619850195c095e59aee59f5b0aacf4
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-06 15:58:15 +0100

    Plotting with moving average

--- src/plot.py
+++ src/plot.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_rewards(csv_file_path):
+    rolling_window = 50
     data = pd.read_csv(csv_file_path)
+    data['Reward_MA'] = data['Reward'].rolling(rolling_window).mean()
 
     plt.figure(figsize=(10,5))
-    plt.plot(data['Episode'], data['Reward'], label = 'Reward per Episode')
-    plt.xlabel('Steps')
+    plt.plot(data['Episode'], data['Reward'], label = 'Reward per Episode', color = 'blue', alpha = 0.3)
+    plt.plot(data['Episode'], data['Reward_MA'], label = f'{rolling_window}-Episode Moving Average', color = 'red')
+    plt.xlabel('Episodes')
     plt.ylabel('Reward')
-    plt.title('Rewards over Steps')
+    plt.title('Rewards over Episodes')
     plt.legend()
     plt.show()
 
