a01f8a57aa701125d8e9a0607fb2cc2f902eacf8
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-07 14:46:22 +0100

    Started on DQN

--- src/agents/DQN.py
+++ src/agents/DQN.py
@@ -1,3 +1,45 @@
+'''Mainly coded with help of Pytorch: https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html'''
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F 
+
+env = gym.make("CartPole-v1")
+
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+device = torch.device("cuda" if torch.cude.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen = capacity)
+    
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    
+    def __len__(self):
+        return len(self.memory)
+    
 
 
-# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
+
