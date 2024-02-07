53df0b557e987cdb15d1428a6011c6a2c3db1038
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-07 20:18:44 +0100

    Working on DQN!

--- src/agents/DQN.py
+++ src/agents/DQN.py
@@ -39,7 +39,80 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
     
+class DQN(nn.module):
 
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
 
+    def forward(self, x):
+        x = F.relu(self.layer(x))
+        x = F.relu(self.layer(x))
+        return self.layer3(x)
+    
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
 
+n_actions = env.action_space.n
+
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state(dict())) # Ensures target's and policy's weights are same.
+
+optimizer = optim.Adamw(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    esp_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1 * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > esp_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1).indices.view(1,1)
+    else:
+        return torch.tensor([[env.action_space.sample()]],
+                            device = device, dtype=torch.long)
+    
+
+episode_durations = []
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+    
+    plt.pause(0.001)
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+        else:
+            display.display(plt.gcf())
+            
 
