a2df4886fedb499f9cf0f3ba1d3a9682f9ced692
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-08 09:03:10 +0100

    DQN learns!

--- src/agents/DQN.py
+++ src/agents/DQN.py
@@ -20,7 +20,7 @@ if is_ipython:
 
 plt.ion()
 
-device = torch.device("cuda" if torch.cude.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -39,7 +39,7 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
     
-class DQN(nn.module):
+class DQN(nn.Module):
 
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
@@ -48,8 +48,8 @@ class DQN(nn.module):
         self.layer3 = nn.Linear(128, n_actions)
 
     def forward(self, x):
-        x = F.relu(self.layer(x))
-        x = F.relu(self.layer(x))
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
         return self.layer3(x)
     
 BATCH_SIZE = 128
@@ -65,11 +65,15 @@ n_actions = env.action_space.n
 state, info = env.reset()
 n_observations = len(state)
 
+'''The policy_net uses a NN to approximate the Q-value function. Input to this network is the state
+   of the environment, and output are the predicted Q-values for each possible action in that state.'''
 policy_net = DQN(n_observations, n_actions).to(device)
+'''The target_net is a clone of the policy network, but with frozen weights. The weights are updated
+   less frequently, which helps stabilize learning by providing a stable target for the Q-values updates.'''
 target_net = DQN(n_observations, n_actions).to(device)
-target_net.load_state_dict(policy_net.state(dict())) # Ensures target's and policy's weights are same.
+target_net.load_state_dict(policy_net.state_dict()) # Ensures target's and policy's weights are same.
 
-optimizer = optim.Adamw(policy_net.parameters(), lr=LR, amsgrad=True)
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
 memory = ReplayMemory(10000)
 
 steps_done = 0
@@ -114,5 +118,78 @@ def plot_durations(show_result=False):
             display.clear_output(wait=True)
         else:
             display.display(plt.gcf())
-            
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)),
+                                            device = device, dtype = torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = \
+        target_net(non_final_next_states).max(1).values
+        
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+if torch.cuda.is_available():
+    num_episodes = 600
+else:
+    num_episodes = 50
+
+for episode in range(num_episodes):
+    state, info = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        if terminated:
+            next_state = None
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+        memory.push(state, action, next_state, reward)
+        state = next_state
+        optimize_model()
+
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * TAU + target_net_state_dict[key] * (1-TAU)
+            target_net.load_state_dict(target_net_state_dict)
+
+        if terminated or truncated:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+
+print('Complete')
+plot_durations(show_result=True)
+plt.ioff()
+plt.show()
+
 
